9faea3644f5f16882490700d68a95bfc66a1b0b8
Mustafa <mustafa.moiz125@gmail.com>
2020-07-14 03:32:43 -0700

    add input error handling and replay mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use std::io::{stdin, stdout, Write};
 
+fn ask_user_for_replay() -> bool {
+    let mut replay = String::new();
+    println!("");
+    print!("The math, it beguiles you. Do you wish to play again (y/n)? ");
+    stdout().flush().unwrap();
+
+    stdin()
+        .read_line(&mut replay)
+        .expect("No replay input read.");
+
+    let first = replay
+        .chars()
+        .next()
+        .expect("Couldn't extract a character.");
+    if first != 'y' {
+        println!("I see. I will look forward to our next encounter then, O Great One. Until then.");
+        false
+    } else {
+        true
+    }
+}
+
 fn main() {
     loop {
         let mut a = String::new();
         let mut b = String::new();
 
+        // Clear screen
         print!("{}[2J", 27 as char);
 
         // Get first input
@@ -22,29 +45,23 @@ fn main() {
         stdin().read_line(&mut b).expect("Failed to read input.");
         let b = b.trim_end();
 
-        // Do some error checking here
-
-        // If valid input, give sum
-        println!("Thank you for your numbers and your patience, O Great One!\nBehold:");
-        println!("\n\t{} + {} = {}", a, b, "5");
-        stdout().flush().unwrap();
+        // Respond depending on validity of inputs
+        let a = a.parse::<i32>();
+        let b = b.parse::<i32>();
+        if a.is_err() || b.is_err() {
+            println!("O Sly One, it looks like you have provided me with improper inputs.");
+            println!("Addition under these conditions would be futile.");
+        } else {
+            let a = a.unwrap();
+            let b = b.unwrap();
+            println!("Thank you for providing me such immaculate numbers, O Great One!\nBehold, now, the power of summation:");
+            println!("\n\t{} + {} = {}", a, b, a + b);
+            stdout().flush().unwrap();
+        }
 
         // Ask user if they want to replay, otherwise break
-        let mut replay = String::new();
-        println!("");
-        print!("The math, it beguiles you. Do you wish to play again (y/n)? ");
-        stdout().flush().unwrap();
-
-        stdin()
-            .read_line(&mut replay)
-            .expect("No replay input read.");
-
-        let first = replay
-            .chars()
-            .next()
-            .expect("Couldn't extract a character.");
-        if first != 'y' {
-            println!("I see. I will look forward to our next encounter then, O Great One. Until then.");
+        let wants_replay = ask_user_for_replay();
+        if !wants_replay {
             break;
         }
     }
